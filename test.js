9202f75e27d561eed4c67b36bd84d7539aa93fb5
Robert Sirois <eidy@Eidy.local>
2017-03-03 11:01:58 -0700

    Removed node_modules. Updated test.js to print out NMEA port data.

--- test.js
+++ test.js
@@ -1,8 +1,35 @@
 var sp = require( 'serialport' )
 var nmea = require( 'nmea' )
 
-sp.list( function( err, ports ) {
-    ports.forEach( function( port ) {
-        console.log( port )
+const argv = require( 'yargs' )
+    .default( 'port', 'COM4' )
+    .default( 'samples', null )
+    .default( 'list', false )
+    .argv
+
+if ( argv.list ) {
+    sp.list( function( err, ports ) {
+        ports.forEach( function( port ) {
+            console.log( port )
+        })
     })
+}
+
+var port = new sp.SerialPort( argv.port, {
+    baudrate: 9600,
+    parser: sp.parsers.readline( '\r\n' )
 })
+
+var countSamples = argv.samples
+
+port.on( 'data', function( line ) {
+    if ( if typeof argv.samples == 'number' ) {
+        if ( countSamples > 0 ) print( line )
+    } else {
+        print( line )
+    }
+})
+
+function print( line ) {
+    console.log( nmea.parse( line ) )
+}
