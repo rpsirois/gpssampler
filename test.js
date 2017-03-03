ebd82b82865bb02894bb0afbe674150fa6a6d867
Robert Sirois <eidy@Eidy.local>
2017-03-03 12:33:11 -0700

    Print out full data points.

--- test.js
+++ test.js
@@ -1,9 +1,10 @@
+var async = require( 'async' )
 var sp = require( 'serialport' )
 var nmea = require( 'nmea' )
 
 const argv = require( 'yargs' )
-    .default( 'port', 'COM4' )
-    .default( 'samples', null )
+    .default( 'nmeaPort', 'COM4' )
+    .default( 'commPort', 'COM6' )
     .default( 'list', false )
     .argv
 
@@ -15,21 +16,86 @@ if ( argv.list ) {
     })
 }
 
-var port = new sp.SerialPort( argv.port, {
+var commPort = new sp.SerialPort( argv.commPort, {
     baudrate: 9600,
     parser: sp.parsers.readline( '\r\n' )
 })
 
-var countSamples = argv.samples
+var lastCommData
+commPort.on( 'data', function( line ) {
+    lastCommData = line
+})
 
-port.on( 'data', function( line ) {
-    if ( typeof argv.samples == 'number' ) {
-        if ( countSamples > 0 ) print( line )
-    } else {
-        print( line )
+var nmeaPort = new sp.SerialPort( argv.nmeaPort, {
+    baudrate: 9600,
+    parser: sp.parsers.readline( '\r\n' )
+})
+
+var lat, lon, alt, updated, csq
+
+nmeaPort.on( 'data', function( line ) {
+    var data = parse( line )
+    if ( data && data.sentence == 'GGA' ) {
+        lat = nmeaToDecimal( data.lat, data.latPole )
+        lon = nmeaToDecimal( data.lon, data.lonPole )
+        alt = data.alt
+        updated = new Date()
+
+        updateCsq()
     }
 })
 
-function print( line ) {
-    console.log( nmea.parse( line ) )
+function updateCsq() {
+    commPort.write( 'AT+CSQ' ).drain( function( err ) {
+        if ( err ) {
+            console.log( err )
+        } else {
+            csq = lastCommData
+        }
+    })
+}
+
+function parse( line ) {
+    try {
+        return nmea.parse( line )
+    } catch( err ) {
+        return null
+    }
+}
+
+function nmeaToDecimal( dmStr, dir ) {
+    var idx = dmStr.indexOf( '.' ) - 2
+    if ( idx < 0 ) idx = 0
+
+    var minutes = Number( dmStr.substr( idx ) )
+    var degrees = Number( dmStr.substr( 0, idx ) )
+    var decimal = degrees + ( minutes / 60 )
+
+    if ( dir == 'S' || dir == 'W' ) decimal = decimal * -1
+
+    return decimal
+}
+
+function main( host='localhost', port='8125' ) {
+    var clock = (new Date()).getTime()
+
+    async.forever(
+        function( next ) {
+            var cur = (new Date()).getTime()
+            //if ( cur >= ( clock + 300000 ) ) { // five minutes
+            if ( cur >= ( clock + 5000 ) ) { // five seconds
+                clock = cur
+                console.log(`
+                    \n===== SAMPLE =====\n
+                    ${ updated }\n
+                    Lat:\t${ lat }\n
+                    Lon:\t${ lon }\n
+                    Alt:\t${ alt }\n
+                    Csq:\t${ csq }
+                `)
+            }
+        },
+        function( err ) {
+            console.log( err )
+        }
 }
